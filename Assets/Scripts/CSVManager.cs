acb3ec4737fd09c989ee9378117ddcff4690ec2a
I. Rasuna Khatami <rasunaitawa@gmail.com>
2017-04-30 14:31:39 -0500

    Added Script
    CSV Manager and Game Manager

---
+++ Assets/Scripts/CSVManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CSVEditor
+{
+    class CSVManager
+    {
+        public struct Record
+        {
+            public string name;
+            public string score;
+        }
+
+        static string leaderboardDataPath = @"Data\Data1.dat";
+
+        private static List<Record> loadLeaderboard()
+        {
+            List<Record> leaderboard = new List<Record>();
+
+            // Error caused when the code could not locate target file is now fixed.
+            if (!Directory.Exists(@"Data\")) Directory.CreateDirectory(@"Data\");
+            if (!File.Exists(leaderboardDataPath)) saveLeaderboard(leaderboard);
+
+            using (StreamReader sr = new StreamReader(leaderboardDataPath))
+            {
+                string headerLine = sr.ReadLine();
+                string line = "";
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] tokens = line.Split(';');
+
+                    var r = new Record();
+                    r.name = tokens[0];
+                    r.score = tokens[1];
+
+                    leaderboard.Add(r);
+                }
+            }
+
+            return leaderboard;
+        }
+
+        private static void saveLeaderboard(List<Record> oldLeaderboard)
+        {
+            var leaderboard = oldLeaderboard.OrderByDescending(x => int.Parse(x.score));
+
+            using (FileStream fs = new FileStream(leaderboardDataPath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine("name;score");
+
+                    foreach(var r in leaderboard)
+                    {
+                        writer.WriteLine(r.name + ";" + r.score);
+                    }
+                }
+            }
+        }
+
+        public static void addRecord(string name, int score)
+        {
+            var leaderboard = loadLeaderboard();
+
+            var r = new Record();
+            r.name = name;
+            r.score = score.ToString();
+            leaderboard.Add(r);
+
+            saveLeaderboard(leaderboard);
+        }
+
+        public static List<Record> getTop10Leaderboard()
+        {
+            var leaderboard = loadLeaderboard();
+            var top10 = new List<Record>();
+            var count = 0;
+
+            foreach(var r in leaderboard)
+            {
+                if (count >= 10) break;
+
+                top10.Add(r); count++;
+            }
+
+            // Add dummy records to complete 10 list.
+            for (int i = 0; i < 10 - count; i++)
+            {
+                var dummy = new Record();
+                dummy.name = null;
+                dummy.score = null;
+
+                top10.Add(dummy);
+            }
+
+            return top10;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject spawnerPrefab;
+    public static bool isGamePaused;
+    public static bool isGameOver;
+
+    private GameObject gameOverCanvas;
+    private GameObject pauseCanvas;
+    private GameObject inGameCanvas;
+    private AfterGameStuff afterGameStuff;
+
+    [HideInInspector]
+    public static int enemiesAlive;
+    public static int score;
+    static Text scoreText;
+    static Text enemiesAliveText;
+    static Text nextWaveText;
+
+    public float timeBetweenWaves = 5f;
+
+    GameObject[] enemySpawners;
+
+    int wave;
+    int waveSize;
+    float timePassedSinceWave;
+
+    void Awake()
+    {
+        gameOverCanvas = GameObject.Find("GameOverCanvas");
+        pauseCanvas = GameObject.Find("PauseCanvas");
+        inGameCanvas = GameObject.Find("InGameCanvas");
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        enemiesAliveText = GameObject.Find("EnemiesText").GetComponent<Text>();
+        nextWaveText = GameObject.Find("NextWaveText").GetComponent<Text>();
+        afterGameStuff = GetComponent<AfterGameStuff>();
+
+        isGameOver = false;
+        isGamePaused = false;
+
+        score = 0;
+        enemiesAlive = 0;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        FindEnemySpawners();
+
+        pauseCanvas.SetActive(false);
+        gameOverCanvas.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        // Just in case
+        Time.timeScale = 1f;
+
+        wave = 0;
+        timePassedSinceWave = 0f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isGamePaused)
+            {
+                if(!isGameOver)
+                {
+                    ResumeGame();
+                }
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+
+        // If no enemy left, start spawning the new wave
+        if(enemiesAlive == 0)
+        {
+            timePassedSinceWave += Time.deltaTime;
+
+            nextWaveText.text = "NEXT WAVE IN " + Mathf.CeilToInt(timeBetweenWaves - timePassedSinceWave).ToString() + "!";
+            nextWaveText.enabled = true;
+
+            if (timePassedSinceWave >= timeBetweenWaves)
+            {
+                wave++;
+                waveSize = CalculateWaveSize(wave);
+
+                for (int i = 0; i < waveSize; i++)
+                {
+                    SpawnEnemy(Random.Range(0, enemySpawners.Length - 1));
+                }
+
+                timePassedSinceWave = 0f;
+            }
+        }
+        else
+        {
+            nextWaveText.enabled = false;
+        }
+	}
+
+    int CalculateWaveSize(int currentWave)
+    {
+        if (currentWave <= 2)
+            return currentWave;
+        if (currentWave <= 10)
+            return (currentWave-1) * 2;
+        return (currentWave - 10) + 20;
+    }
+
+    void FindEnemySpawners()
+    {
+        enemySpawners = GameObject.FindGameObjectsWithTag("EnemySpawner");
+    }
+
+    void SpawnEnemy(int spawnerIndex)
+    {
+        Object prefab = spawnerPrefab as Object;
+        Vector3 pos = enemySpawners[spawnerIndex].transform.position;
+        Quaternion rot = spawnerPrefab.transform.rotation;
+
+        Object.Instantiate(prefab, pos, rot);
+
+        enemiesAlive++;
+        SetEnemiesText();
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        isGamePaused = true;
+        isGameOver = true;
+
+        afterGameStuff.SetGameOverPanels();
+
+        gameOverCanvas.SetActive(true);
+        inGameCanvas.SetActive(false);
+
+        // Cursor settings
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isGamePaused = true;
+
+        pauseCanvas.SetActive(true);
+
+        // Cursor settings
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isGamePaused = false;
+
+        pauseCanvas.SetActive(false);
+
+        // Cursor settings
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public static void IncreaseScore(int amount)
+    {
+        score += amount;
+
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+
+    public static void SetEnemiesText()
+    {
+        enemiesAliveText.text = "ENEMIES ALIVE: " + enemiesAlive.ToString();
+    }
+}
