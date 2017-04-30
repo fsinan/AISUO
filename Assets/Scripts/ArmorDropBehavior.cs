fd56a2e62f783b3f15a0895f439728de07dc3b9b
Omer Faruk Yolal <oyolal@na.edu>
2017-04-30 14:34:40 -0500

    Armor drop and Camera Facing

---
+++ Assets/Scripts/ArmorDropBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorDropBehavior : MonoBehaviour {
+
+    public float revSpeed = 45f;
+    public int armorAmount = 50;
+    public float lifeSpan = 5f;
+
+    private float lifeTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lifeTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.up, revSpeed * Time.deltaTime);
+
+        lifeTime += Time.deltaTime;
+
+        if(lifeTime >= lifeSpan)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && other.gameObject.GetComponent<PlayerHealth>().armor < other.gameObject.GetComponent<PlayerHealth>().maxArmor)
+        {
+            other.gameObject.GetComponent<PlayerHealth>().AddArmor(armorAmount);
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFacing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacing : MonoBehaviour {
+
+	public Camera lookCamera;
+
+	// Use this for initialization
+	void Start () 
+	{
+        lookCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt(transform.position + lookCamera.transform.rotation * Vector3.forward,
+			lookCamera.transform.rotation * Vector3.up);
+	}
+}
